322898de55dec2c57080b86da90d60d59d13c287
Arutosio <Arutosio@outlook.com>
2019-08-20 22:28:43 +0200

    minor fix

--- Program.cs
+++ Program.cs
@@ -11,22 +11,21 @@ namespace Corgdirile
     {
         static void Main(string[] args)
         {
+            //Fase Preparing..
             Console.WriteLine("AHAHAHAHAHAAHAHAHAHAHAHA");
             string pathSource = SetDirectory("Write the path where will get all files: ");
             string pathDestination = SetDirectory("Inserisci il nome della directori di destinazione: ");
             //string[] allfiles = Directory.GetFileSystemEntries(pathSource);
             string[] allfiles = Directory.GetFiles(pathSource, "*", SearchOption.AllDirectories);
+            // Fase Processing..
             for (int i = 0; i < allfiles.Length; i++) {
                 string fileName = Path.GetFileName(allfiles[i]);
                 string yearFile = File.GetCreationTime(allfiles[i]).Year.ToString();
                 string monthFile = File.GetCreationTime(allfiles[i]).Month.ToString();
-                Console.Write($"Num: {0} - Filename: ",i);ColorsLines.WriteLineC(fileName,Cyan);
+                Console.Write($"File Num: {i+1} - Name: ");ColorsLines.WriteLineC(fileName,Cyan);
                 CreateFolder(pathDestination, yearFile);
-                Console.WriteLine(Path.Combine(pathDestination, yearFile));
                 CreateFolder(Path.Combine(pathDestination, yearFile), yearFile+"-"+monthFile);
                 MoveFile(allfiles[i], fileName, Path.Combine(pathDestination, yearFile, yearFile+"-"+monthFile)); // PathTooLongException move file
-            /*
-            */
             }
         }
         public static string SetDirectory(string dir) {
@@ -44,14 +43,14 @@ namespace Corgdirile
             {
                 // Determine whether the directory exists.
                 if (!Directory.Exists(Path.Combine(path, name))) {
-                    Console.Write("Creating folder in: "); ColorsLines.WriteLineC(path, DarkYellow);
-                    Console.Write("Name: "); ColorsLines.WriteC(name, Yellow);
+                    Console.Write("    Creating folder in: "); ColorsLines.WriteLineC(path, DarkYellow);
+                    Console.Write("    Name: "); ColorsLines.WriteC(name, Yellow);
                 }
                 else return;
 
                 // Try to create the directory.
                 DirectoryInfo di = Directory.CreateDirectory(Path.Combine(path, name));
-                Console.Write(" - ");ColorsLines.WriteC("Done!", Green);Console.Write(" - In the date: ");
+                Console.Write(" - ");ColorsLines.WriteC("Done!", Green);Console.Write(" - In the: ");
                 ColorsLines.WriteLineC(Directory.GetCreationTime(Path.Combine(path, name)).ToString(), Magenta);
 
                 // Delete the directory.
@@ -61,16 +60,18 @@ namespace Corgdirile
             catch (Exception e)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write("The process failed: {0}"); Console.WriteLine(e.ToString());
+                Console.Write($"The process failed: {0}",e); Console.WriteLine(e.ToString());
                 Console.ResetColor();
             }
             finally { }
         }
-        public static string MoveFile(string filePath, string fileName,string path) {
-            Console.Write("Moving File: "); ColorsLines.WriteLineC(filePath, Yellow);
-            File.Move(filePath, Path.Combine(path, fileName)); // PathTooLongException move file
-            Console.Write("...  "); ColorsLines.WriteLineC("Done!", Green);
-            return path;
+        public static string MoveFile(string filePath, string fileName, string newPath) {
+            Console.Write("    Moving File: "); 
+            ColorsLines.WriteLineC(filePath, DarkYellow);
+            File.Move(filePath, Path.Combine(newPath, fileName)); // PathTooLongException move file
+            Console.Write("   in to "); ColorsLines.WriteC(newPath+'/', Yellow); ColorsLines.WriteC(fileName, Cyan);
+            Console.Write(" ... "); ColorsLines.WriteLineC("Done!\r\n", Green);
+            return newPath;
         }
     }
 }
