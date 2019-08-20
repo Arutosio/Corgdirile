ac6e88199a02cedc59d910635e1ecb51d2ab99f2
Arutosio <Arutosio@outlook.com>
2019-08-20 14:50:47 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Corgdirile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
